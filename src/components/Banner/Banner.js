69dbd744309950c08f2c1d5ddc1104983876c5d1
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2020-05-23 20:21:10 -0300

    Adding paralax effect on page

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,11 +4,11 @@ import './Banner.css'
 export default class Banner extends Component {
   render() {
     return (
-      <div className="banner">
+      <div className="banner" id="home">
         <div className="banner-img">
           <h1 className="banner-text">
             <p>Hi, my name is Rodrigo Martins.</p>
-            <p>I'm junior Full-stack developer and this is my portfolio</p>
+            <p>I'm a Full-stack developer and this is my portfolio</p>
           </h1>
         </div>
       </div>
