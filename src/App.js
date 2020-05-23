757a940fcb802e284dfc90c3cc8a2275ecd913d8
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2020-05-23 20:26:44 -0300

    Adding About section to page and background secondary color

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Banner from './components/Banner/Banner';
+import Section from './components/Section/Section';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Banner></Banner>
+      <Section></Section>
     </div>
   );
 }
 
-// TODO put credits for background image Photo by Ave Calvar on Unsplash
+// TODO put credits for background image: "Photo by Ave Calvar on Unsplash"
 
 export default App;
